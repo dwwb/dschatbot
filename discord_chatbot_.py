00c52c505428538250bd9cc60ff7551a3c33080c
dwwb <31266925+dwwb@users.noreply.github.com>
2019-04-15 01:27:45 +0300

    Update discord_chatbot_.py

--- discord_chatbot_.py
+++ discord_chatbot_.py
@@ -50,25 +50,25 @@ if __name__ == '__main__':
 	scheduler = AsyncIOScheduler()
 	#Кзарка Нубер
 	scheduler.add_job(boss_scheduler, 'cron', (0, 4), day_of_week="sun", hour=17, minute=40)
-	scheduler.add_job(boss_scheduler, 'cron', (0, 4), day_of_week="fri", hour=23, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (0, 4), day_of_week="fri", hour=23, minute=40)
 	#Кзарка Каранда
-	scheduler.add_job(boss_scheduler, 'cron', (0, 1), day_of_week="sun", hour=23, minute=10)
-	#Карнда Нубера
-	scheduler.add_job(boss_scheduler, 'cron', (1, 4), day_of_week="mon", hour=22, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (0, 1), day_of_week="sun", hour=23, minute=40)
+	#Карaнда Нубер
+	scheduler.add_job(boss_scheduler, 'cron', (1, 4), day_of_week="mon", hour=22, minute=40)
 	#Каранда Кутум
-#	scheduler.add_job(boss_scheduler, 'cron', (1, 3), day_of_week="sat", hour=23, minute=10)
+	# scheduler.add_job(boss_scheduler, 'cron', (1, 3), day_of_week="sat", hour=00, minute=27)
 	#Квинт Мурака
 	scheduler.add_job(boss_scheduler, 'cron', (5, 6), day_of_week="sat", hour=13, minute=40)
-	scheduler.add_job(boss_scheduler, 'cron', (5, 6), day_of_week="tue", hour=22, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (5, 6), day_of_week="tue", hour=22, minute=40)
 	#Офин
-	scheduler.add_job(boss_scheduler, 'cron', (2, 8), day_of_week="wed, fri, sun", hour=22, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (2, 8), day_of_week="wed, fri, sun", hour=22, minute=40)
 	#Нубер
 	scheduler.add_job(boss_scheduler, 'cron', (4, 8), day_of_week="mon, wed", hour=0, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (4, 8), day_of_week="sat", hour=9, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (4, 8), day_of_week="thu", hour=11, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (4, 8), day_of_week="mon", hour=13, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (4, 8), day_of_week="wed, thu, fri", hour=15, minute=40)
-	scheduler.add_job(boss_scheduler, 'cron', (4, 8), day_of_week="thu", hour=23, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (4, 8), day_of_week="thu", hour=23, minute=40)
 	#Кутум
 	scheduler.add_job(boss_scheduler, 'cron', (3, 8), day_of_week="thu, sat", hour=0, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (3, 8), day_of_week="sun", hour=9, minute=40)
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 	scheduler.add_job(boss_scheduler, 'cron', (3, 8), day_of_week="thu, fri, sun", hour=13, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (3, 8), day_of_week="tue", hour=15, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (3, 8), day_of_week="mon, sat", hour=17, minute=40)
-	scheduler.add_job(boss_scheduler, 'cron', (3, 8), day_of_week="tue", hour=23, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (3, 8), day_of_week="tue", hour=23, minute=40)
 	#Каранда
 	scheduler.add_job(boss_scheduler, 'cron', (1, 8), day_of_week="tue", hour=0, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (1, 8), day_of_week="sun", hour=7, minute=40)
@@ -84,20 +84,20 @@ if __name__ == '__main__':
 	scheduler.add_job(boss_scheduler, 'cron', (1, 8), day_of_week="wed", hour=13, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (1, 8), day_of_week="mon, sat", hour=15, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (1, 8), day_of_week="tue", hour=17, minute=40)
-	scheduler.add_job(boss_scheduler, 'cron', (1, 8), day_of_week="wed", hour=23, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (1, 8), day_of_week="wed", hour=23, minute=40)
 	#Кзарка
 	scheduler.add_job(boss_scheduler, 'cron', (0, 8), day_of_week="fri, sun", hour=0, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (0, 8), day_of_week="sat", hour=7, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (0, 8), day_of_week="tue", hour=13, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (0, 8), day_of_week="wed, thu, fri", hour=17, minute=40)
-	scheduler.add_job(boss_scheduler, 'cron', (0, 8), day_of_week="mon", hour=23, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (0, 8), day_of_week="mon", hour=23, minute=40)
 	#Велл
 	scheduler.add_job(boss_scheduler, 'cron', (7, 8), day_of_week="sun", hour=15, minute=40)
 	#Телега
 	scheduler.add_job(telega, 'cron', day_of_week="sat", hour=16, minute=30)
 	#Камос
-	scheduler.add_job(boss_scheduler, 'cron', (9, 8), day_of_week="thu", hour=22, minute=10)
-	scheduler.add_job(boss_scheduler, 'cron', (9, 8), day_of_week="sat", hour=23, minute=10)
+	scheduler.add_job(boss_scheduler, 'cron', (9, 8), day_of_week="thu", hour=22, minute=40)
+	scheduler.add_job(boss_scheduler, 'cron', (9, 8), day_of_week="sat", hour=23, minute=40)
 	scheduler.add_job(boss_scheduler, 'cron', (9, 8), day_of_week="sun", hour=11, minute=40)
 	scheduler.start()
 		
